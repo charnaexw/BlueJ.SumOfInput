4656c371f68deab81e9c81b526d997f82e7b67e0
charnaexw <charnaexw@gmail.com>
2021-06-26 20:16:41 -0400

    Gave it a shot.

--- SumOfInput.java
+++ SumOfInput.java
@@ -2,25 +2,28 @@
  * Write a description of class SumOfInput here.
  *
  * @author (your name)
- * @version (a version number or a date)
+ * @version (the date and year)
  */
 public class SumOfInput
 {
     // instance variables - replace the example below with your own
-    private Integer sum;
-
+    private Integer output;
     /**
      * Constructor for objects of class SumOfInput
      */
     public SumOfInput()
     {
         // initialise instance variables
-        sum = 0;
+        int sum=0;
+        
     }
     
     public Integer oneToNumber(Integer numberToSum){
-
-        return sum;
+        int numberToSum1=0;
+        for(int i=1; i<= numberToSum; i++){
+        numberToSum1 = numberToSum1 +i; 
+        }
+        return numberToSum1;
     }
 
 }
